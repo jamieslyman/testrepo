c0322226d83c4566574442ea9d92b6e6ca449c61
wdcook4 <wdcook4@gmail.com>
2019-08-17 19:59:07 -0400

    Added chooseone.py

---
+++ chooseone.py
@@ -0,0 +1,9 @@
+inp = input("Choose a number between 1 and 3>")
+if int(inp) == 1:
+    print("He-Hewwo???")
+elif int(inp) == 2:
+    print("OwO What's this???")
+elif int(inp) == 3:
+    print("THAT'S THE WRONG NUMBUH! OHHHHHH")
+else:
+    print("Invalid input, try again (rerun)")
