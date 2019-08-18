ada71a648436392c6b42440fa132af7c36d4467f
wdcook4 <wdcook4@gmail.com>
2019-08-17 20:11:13 -0400

    Added error checking in chooseone.py

--- chooseone.py
+++ chooseone.py
@@ -1,9 +1,12 @@
 inp = input("Choose a number between 1 and 3>")
-if int(inp) == 1:
-    print("He-Hewwo???")
-elif int(inp) == 2:
-    print("OwO What's this???")
-elif int(inp) == 3:
-    print("THAT'S THE WRONG NUMBUH! OHHHHHH")
-else:
-    print("Invalid input, try again (rerun)")
+try:
+    if int(inp) == 1:
+        print("He-Hewwo???")
+    elif int(inp) == 2:
+        print("OwO What's this???")
+    elif int(inp) == 3:
+        print("THAT'S THE WRONG NUMBUH! OHHHHHH")
+    else:
+        print("Invalid input, try again (rerun)")
+except ValueError:
+    print("Please type a number instead")
