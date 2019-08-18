3518b397811a23d93e1ef3c48f256b820890253b
rainbow-rawr <ffletchera1372@gmail.com>
2019-08-17 20:21:48 -0400

    o

--- oof.py
+++ oof.py
@@ -1,11 +1,14 @@
 inp = input ("choose a number betweem 1 and 4>")
-if int(inp) == 1:
-    print("rAwR sMoL bOi")
-elif int(inp) == 2:
-    print("hello smol child") 
-elif int(inp) == 3:
-    print("r a i n b o w s")     
-elif int(inp) == 4:
-    print("wrong.")
-else:
-    print("you forked up")
+try:
+    if int(inp) == 1:
+        print("rAwR sMoL bOi")
+    elif int(inp) == 2:
+        print("hello smol child") 
+    elif int(inp) == 3:
+        print("r a i n b o w s")     
+    elif int(inp) == 4:
+        print("wrong.")
+    else:
+        print("you forked up")
+except ValueError:
+    print("NUMBORS U HOE")
